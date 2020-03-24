60b3c232de86dbb105a7d9805d546adc7b732c9b
Katia Ramaniuk <katia-l@mail.ru>
2020-03-24 23:37:42 +0300

    Added mongodb, changed methods

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
 const app = express();
+let db;
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,29 +26,53 @@ let artists = [
     }
 ];
 
+MongoClient.connect("mongodb://127.0.0.1:27017/mydb", function(err, database){
+    if (err) {
+        return console.log(err);
+    }
+    db = database;
+    app.listen(8080, function() {
+        console.log("API started");
+    });
+});
+
+
 app.get("/", function(req, res) {
     res.send("Hello API");
 });
 
 app.get("/artists", function(req, res) {
-    res.send(artists);
+    db.collection("artists").find().toArray(function(err, docs) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(docs);
+    });
 });
+   
 
 app.get("/artists/:id", function(req, res) {
-    console.log(req.params);
-    const artist = artists.find((oArtist) => {
-        return oArtist.id === Number(req.params.id);
-    })
-    res.send(artist)
+    db.collection("artists").findOne({_id: ObjectId(req.params.id)}, function(err, doc){
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(doc);
+    });
 });
 
 app.post("/artists", function(req, res) {
     const artist = {
-        id: Date.now(),
-        name: req.body.name
+         name: req.body.name
     };
-    artists.push(artist);
-    res.send(artist);
+    db.collection("artists").insert(artist, function(err, result) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(artist);
+    });
 });
 
 app.put("/artists/:id", function(req, res) {
@@ -60,6 +91,3 @@ app.delete("/artists/:id", function(req, res) {
 });
 
 
-app.listen(8080, function() {
-    console.log("API started");
-});
