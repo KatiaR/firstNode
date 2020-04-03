88ac0c2daf4bf99e91fb6885aa16b53253fd2a70
Katia Ramaniuk <katia-l@mail.ru>
2020-04-03 23:08:37 +0300

    added models and controllers

---
+++ models/artists.js
@@ -0,0 +1,43 @@
+const db = require('../db');
+const ObjectId = require('mongodb').ObjectID;
+
+exports.all = function (cb) {
+	db.get()
+		.collection('artists')
+		.find()
+		.toArray(function (arr, docs) {
+			cb(arr, docs);
+		});
+};
+
+exports.findById = function (id, cb) {
+	db.get()
+		.collection('artists')
+		.findOne({ _id: ObjectId(id) }, function (err, doc) {
+			cb(err, doc);
+		});
+};
+
+exports.create = function (artist, cb) {
+	db.get()
+		.collection('artists')
+		.insert(artist, function (err, result) {
+			cb(err, result);
+		});
+};
+
+exports.update = function (id, newData, cb) {
+	db.get()
+		.collection('artists')
+		.updateOne({ _id: ObjectId(id) }, newData, function (err, result) {
+			cb(err, result);
+		});
+};
+
+exports.delete = function (id, cb) {
+	db.get()
+		.collection('artists')
+		.deleteOne({ _id: ObjectId(id) }, function (err, result) {
+			cb(err, result);
+		});
+};
--- server.js
+++ server.js
@@ -4,98 +4,26 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 const db = require('./db');
+const artistsController = require('./controllers/artists');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let artists = [
-	{
-		id: 1,
-		name: 'Metallica',
-	},
-	{
-		id: 2,
-		name: 'Iron Maiden',
-	},
-	{
-		id: 3,
-		name: 'Deep Purple',
-	},
-];
-
 app.get('/', function (req, res) {
 	res.send('Hello API');
 });
 
-app.get('/artists', function (req, res) {
-	db.get()
-		.collection('artists')
-		.find()
-		.toArray(function (err, docs) {
-			if (err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.send(docs);
-		});
-});
+app.get('/artists', artistsController.all);
 
-app.get('/artists/:id', function (req, res) {
-	db.get()
-		.collection('artists')
-		.findOne({ _id: ObjectId(req.params.id) }, function (err, doc) {
-			if (err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.send(doc);
-		});
-});
+app.get('/artists/:id', artistsController.findById);
 
-app.post('/artists', function (req, res) {
-	const artist = {
-		name: req.body.name,
-	};
-	db.get()
-		.collection('artists')
-		.insert(artist, function (err, result) {
-			if (err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.send(artist);
-		});
-});
+app.post('/artists', artistsController.create);
 
-app.put('/artists/:id', function (req, res) {
-	db.get()
-		.collection('artists')
-		.updateOne(
-			{ _id: ObjectId(req.params.id) },
-			{ name: req.body.name },
-			function (err, result) {
-				if (err) {
-					console.log(err);
-					return res.sendStatus(500);
-				}
-				res.sendStatus(200);
-			}
-		);
-});
+app.put('/artists/:id', artistsController.update);
 
-app.delete('/artists/:id', function (req, res) {
-	db.get()
-		.collection('artists')
-		.deleteOne({ _id: ObjectId(req.params.id) }, function (err, result) {
-			if (err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.sendStatus(200);
-		});
-});
+app.delete('/artists/:id', artistsController.delete);
 
 db.connect('mongodb://127.0.0.1:27017/mydb', function (err) {
 	if (err) {
